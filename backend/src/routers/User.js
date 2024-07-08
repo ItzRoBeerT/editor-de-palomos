17a833cbebd867684f80ec43be47145a34c389e9
Roberto <robertocaravaca436@gmail.com>
2024-07-08 17:57:45 +0200

    Updated user router

--- backend/src/routers/User.js
+++ backend/src/routers/User.js
@@ -2,13 +2,17 @@ const express = require('express');
 const User = require('../models/User');
 const router = new express.Router();
 
-router.post('user/createAccount', async (req, res) => {
+router.post('/user/createAccount', async (req, res) => {
 	const user = new User(req.body);
 	try {
 		await user.save();
-		res.send('account created');
+		res.status(200).send({
+			message: 'Account created!',
+			user,
+		});
 	} catch (error) {
-		res.status(400).send({ error: e.message });
+		res.status(400).send({ error: error.message });
 	}
 });
+
 module.exports = router;
