41f9b557fd12988dda45c0f805c312d33f53ee0a
Roberto <robertocaravaca436@gmail.com>
2024-07-09 19:52:28 +0200

    hash password , generate tokens and findbycredentials

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
 	name: {
@@ -28,6 +30,48 @@ const userSchema = new mongoose.Schema({
 		trim: true,
 		minlength: 7,
 	},
+	tokens: [
+		{
+			token: {
+				type: String,
+				required: true,
+			},
+		},
+	],
+});
+
+//create token
+userSchema.methods.generateAuthToken = async function () {
+	const user = this;
+	const token = jwt.sign({ _id: user._id.toString() }, 'colombiculturaguadalcacin');
+
+	user.tokens = user.tokens.concat({ token });
+	await user.save();
+	return token;
+};
+
+userSchema.statics.findByCredentials = async (email, password) => {
+	const user = await User.findOne({ email });
+
+	if (!user) {
+		throw new Error('user not found!');
+	}
+	const isMatch = await bcrypt.compare(password, user.password);
+	if (!isMatch) {
+		throw new Error('Passwords do not match!');
+	}
+
+	return user;
+};
+
+//hash plaint text password before saving
+userSchema.pre('save', async function (next) {
+	const user = this;
+	if (user.isModified('password')) {
+		user.password = await bcrypt.hash(user.password, 8);
+	}
+
+	next();
 });
 
 const User = mongoose.model('User', userSchema);
