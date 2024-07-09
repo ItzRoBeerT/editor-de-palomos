f5b3e352c47435b2c06fa3052b7f1a3ebc18e40d
Roberto <robertocaravaca436@gmail.com>
2024-07-09 19:52:48 +0200

    added user login

--- backend/src/routers/User.js
+++ backend/src/routers/User.js
@@ -15,4 +15,14 @@ router.post('/user/createAccount', async (req, res) => {
 	}
 });
 
+router.post('/user/login', async (req, res) => {
+	try {
+		const user = await User.findByCredentials(req.body.email, req.body.password);
+		const token = await user.generateAuthToken();
+		res.send({ user, token });
+	} catch (error) {
+		res.status(400).send({ error: error.message });
+	}
+});
+
 module.exports = router;
