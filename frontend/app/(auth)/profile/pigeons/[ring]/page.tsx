b254d085098bba6326123aa9afdce41a4235f3e3
Roberto <robertocaravaca436@gmail.com>
2024-07-16 13:18:19 +0200

    Added moment to display dates

--- frontend/app/(auth)/profile/pigeons/[ring]/page.tsx
+++ frontend/app/(auth)/profile/pigeons/[ring]/page.tsx
@@ -1,5 +1,7 @@
 import Card from '@/components/containers/card';
 import { getPigeon } from '@/lib/pigeon';
+import { getUser } from '@/lib/user';
+import moment from 'moment';
 import { cookies } from 'next/headers';
 
 interface Props {
@@ -12,15 +14,18 @@ export default async function Pigeon(props: Props) {
 	const { params } = props;
 	const ring = params.ring;
 	const token = cookies().get('token')?.value || '';
+	const user = await getUser(token);
 	const pigeon = await getPigeon(token, ring);
 
 	return (
 		<main>
 			<Card>
-				<h1>{pigeon.name} de </h1>
+				<h1 className="text-4xl text-center">
+					{pigeon.name} de {user.name}
+				</h1>
 			</Card>
 			<p>{pigeon.feather}</p>
-			<p>{pigeon.birthday}</p>
+			<p>{moment(pigeon.birthday).format('DD/MM/YYYY')}</p>
 			<p>{pigeon.ring}</p>
 			<p>test</p>
 		</main>
