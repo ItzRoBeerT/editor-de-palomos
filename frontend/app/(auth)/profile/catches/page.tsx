6f6b2e365328d0e5f92d873f0088de3af7461512
Roberto Caravaca <robertocaravaca436@gmail.com>
2024-07-20 12:51:27 +0200

    Added catches page

---
+++ frontend/app/(auth)/profile/catches/page.tsx
@@ -0,0 +1,8 @@
+export default function CatchesPage() {
+    return (
+        <main>
+            <h1>Bienvenido a la página de cpaturas</h1>
+            <p>Esta página se encuentra todavía en desarrollo!!!</p>
+        </main>
+    )
+}
