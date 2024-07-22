5390eaf6f4f2701628f09903988c64d5a65f725d
Roberto Caravaca <robertocaravaca436@gmail.com>
2024-07-22 17:05:00 +0200

    Added  more info on pigeons page

--- frontend/app/(auth)/profile/catches/page.tsx
+++ frontend/app/(auth)/profile/catches/page.tsx
@@ -1,8 +1,16 @@
+import Catches from '@/components/catches';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'Capturas',
+	description:
+		'Aquí puedes ver todos tus palomos de embreo que estan concursando. Puedes filtrar por año. Además también puedes consultar la puntucación total de capturas.',
+};
+
 export default function CatchesPage() {
-    return (
-        <main>
-            <h1>Bienvenido a la página de cpaturas</h1>
-            <p>Esta página se encuentra todavía en desarrollo!!!</p>
-        </main>
-    )
+	return (
+		<main>
+			<Catches />
+		</main>
+	);
 }
