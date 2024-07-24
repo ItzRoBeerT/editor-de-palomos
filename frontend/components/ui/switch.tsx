ee200cb492737bd3b8f1abae93ad06e2b44667ba
Roberto Caravaca <robertocaravaca436@gmail.com>
2024-07-24 10:38:46 +0200

    Added defaultCheked on switch component

--- frontend/components/ui/switch.tsx
+++ frontend/components/ui/switch.tsx
@@ -12,6 +12,7 @@ export interface SwitchProps {
 export default function Switch(props: SwitchProps) {
 	const { token, pigeon } = props;
 
+	const defaultChecked = pigeon.isCatching;
 	async function onChangeHandler(e: any) {
 		const isChecked = e.target.checked;
 
@@ -35,7 +36,7 @@ export default function Switch(props: SwitchProps) {
 
 	return (
 		<label className="inline-flex items-center cursor-pointer">
-			<input type="checkbox" className="sr-only peer" onChange={onChangeHandler} />
+			<input type="checkbox" defaultChecked={defaultChecked} className="sr-only peer" onChange={onChangeHandler} />
 			<div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
 			<span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
 				Hembreo
